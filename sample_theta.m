501eb6fe89daa221484e73a3a89ca32968d33698
Umberto Picchini <1611907+umbertopicchini@users.noreply.github.com>
2023-07-14 18:25:39 +0200

    Update sample_theta.m

--- sample_theta.m
+++ sample_theta.m
@@ -54,7 +54,7 @@ switch type_margin
         doubleeulergamma=double(eulergamma);
         for i=1:dim
              bet=sqrt(proposal_cov(i,i)*6)/pi;
-             % noticen the MATLAB parameterization of the Gumble distributions is slightly defferent from the canonical one found eg in R or Wikipedia (below we use bet instead of -bet)
+             % notice the MATLAB parameterization of the Gumble distribution is slightly different from the canonical one found eg in R or Wikipedia (below we use bet instead of -bet)
             a = makedist('ExtremeValue','mu', proposal_mean(i)+bet* doubleeulergamma,'sigma',bet);
             theta(i)=icdf(a,AA(i));
             pd=[pd,a];
